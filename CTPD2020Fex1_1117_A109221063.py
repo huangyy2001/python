4a028f3c3c8836f095e226dfb5eb1724eb3b51ca
huangyy2001 <85608530+huangyy2001@users.noreply.github.com>
2023-03-03 23:19:06 +0800

    Update CTPD2020Fex1_1117_A109221063.py

--- CTPD2020Fex1_1117_A109221063.py
+++ CTPD2020Fex1_1117_A109221063.py
@@ -2,7 +2,7 @@
 """
 Created on Sat Nov 28 14:54:07 2020
 
-@author: A109221063
+@author: huangyy
 """
 
 choiceMenu="1:BMI\n2:萬花尺\n-->"
@@ -90,7 +90,7 @@ elif choice==2:
     turtle.goto(-50,-300)
     turtle.pendown()
     turtle.pencolor("white")
-    turtle.write("CT2020F_Ex1Q3\nA109221063_黃雅宜",font=16)   
+    turtle.write("CT2020F_Ex1Q3\nHuangyy",font=16)   
     turtle.done() 
 else:
     print("請輸入1或2")
